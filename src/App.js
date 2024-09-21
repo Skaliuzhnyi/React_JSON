58c57e307d6ca4219093b0abe2bb68cb15df3fa5
Sergey Kaliuzhnyi <flskaliuzhnyi@gmail.com>
2024-09-21 15:42:23 +0200

    add header, nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
+import category from './data/category.json';
+
+
 function App() {
   return (
     <div className="App">
+      <header>
+        <a href="/">Main</a>
+      </header>
 
+      <nav>
+        <ul>
+          {Object.values(category).map(item => <li>{item.name}</li>)}
+        </ul>
+      </nav>
     </div>
   );
 }
